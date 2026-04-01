dd2350947a963ef9a47a0772abf6702e92e65680
pranalYadav <yadav.pranal94@gmail.com>
2026-04-01 10:59:59 +0530

    post api route

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -3,14 +3,55 @@ import Property from '@/models/Property'
 
 //GET/api/properties
 export const GET = async (request) => {
-    try {
-        await connectDB();
+  try {
+    await connectDB();
 
-        const properties = await Property.find()
-        return new Response(JSON.stringify(properties), { status: 200 });
+    const properties = await Property.find()
+    return new Response(JSON.stringify(properties), { status: 200 });
 
-    } catch (error) {
-        console.log(error)
-        return new Response('Something went wrong', { status: 500 })
-    }
+  } catch (error) {
+    console.log(error)
+    return new Response('Something went wrong', { status: 500 })
+  }
 };
+
+export const POST = async (request) => {
+  try {
+    const formData = await request.formData()
+
+    //access aa values  form amenities and images
+    const amenities = formData.getAll('amenities')
+    const images = formData.getAll('images').filter((image) => image.name !== '')
+
+    //create a propertyData object for database
+    const propertyData = {
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+        street: formData.get('location.street'),
+        city: formData.get('location.city'),
+        state: formData.get('location.state'),
+        zipcode: formData.get('location.zipcode'),
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      square_feet: formData.get('square_feet'),
+      amenities,
+      rates: {
+        weekly: formData.get('rates.weekly'),
+        monthly: formData.get('rates.monthly'),
+        nighlty: formData.get('retes.nightly'),
+      },
+      seller_info: {
+        name: formData.get('seller_info.name'),
+        email: formData.get('seller_info.email'),
+        phone: formData.get('seller_info.phone'),
+      },
+      images
+    }
+    return new Response(JSON.stringify({ message: 'Success' }), { status: 200 })
+  } catch (error) {
+    return new Response('Failed to add property', { status: 500 })
+  }
+}
--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -99,7 +99,7 @@ const PropertyAddForm = () => {
 
 
   return mounted &&
-    <form>
+    <form action="/api/properties" method='POST' encType='multipart/form-data'>
       <h2 className="text-3xl text-center font-semibold mb-6">
         Add Property
       </h2>
@@ -553,7 +553,7 @@ const PropertyAddForm = () => {
           Add Property
         </button>
       </div>
-    </form>
+    </form >
 
 }
 
