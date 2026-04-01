c45d0131465dcee9ad64893c33311f7eff5a7114
pranalYadav <yadav.pranal94@gmail.com>
2026-04-01 11:55:08 +0530

    get user and submit to database

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from '@/models/Property'
+import { getSessionUser } from "@/utils/getSessionUser";
 
 //GET/api/properties
 export const GET = async (request) => {
@@ -17,6 +18,17 @@ export const GET = async (request) => {
 
 export const POST = async (request) => {
   try {
+    await connectDB();
+
+
+    const sessionUser = await getSessionUser()
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('User id is required', { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
     const formData = await request.formData()
 
     //access aa values  form amenities and images
@@ -36,22 +48,30 @@ export const POST = async (request) => {
       },
       beds: formData.get('beds'),
       baths: formData.get('baths'),
-      square_feet: formData.get('square_feet'),
+      square_foot: formData.get('square_feet'),
       amenities,
       rates: {
         weekly: formData.get('rates.weekly'),
         monthly: formData.get('rates.monthly'),
-        nighlty: formData.get('retes.nightly'),
+        nighlty: formData.get('rates.nightly'),
       },
       seller_info: {
         name: formData.get('seller_info.name'),
         email: formData.get('seller_info.email'),
         phone: formData.get('seller_info.phone'),
       },
-      images
+      owner: userId,
+      // images
     }
-    return new Response(JSON.stringify({ message: 'Success' }), { status: 200 })
+
+    const newProperty = new Property(propertyData)
+    await newProperty.save()
+
+    return Response.redirect(`${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`)
+
+    // return new Response(JSON.stringify({ message: 'Success' }), { status: 200 })
   } catch (error) {
+    console.log('🔥 FULL ERROR:', error);
     return new Response('Failed to add property', { status: 500 })
   }
 }
